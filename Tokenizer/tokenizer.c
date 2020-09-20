eb6943cd35b576ff7793adc980c10a04517eaf94
Alborz Jelvani <aj654@kill.cs.rutgers.edu>
2020-09-19 20:01:31 -0400

    working on data structures

--- Tokenizer/tokenizer.c
+++ Tokenizer/tokenizer.c
@@ -1,7 +1,19 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+
+
+enum token{word, decimal, octal, hexadecimal, floating, C_op};
+
+struct t_index{//Contains data of where in string token is
+	int start;
+	int end;
+	enum token type;
+}
+
 int main(int argc, char *argv[]) {
 
 	return 0;
 }
+
+int* tokenize
