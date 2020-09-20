3c963cfd317b412415c2ca8d46787b8c7a8defad
Alborz Jelvani <aj654@kill.cs.rutgers.edu>
2020-09-19 21:52:01 -0400

    Added 2 functions that need work

--- Tokenizer/tokenizer.c
+++ Tokenizer/tokenizer.c
@@ -1,19 +1,33 @@
 #include <stdio.h>
 #include <stdlib.h>
-
+#include <ctype.h>
 
 
 enum token{word, decimal, octal, hexadecimal, floating, C_op};
 
 struct t_index{//Contains data of where in string token is
-	int start;
-	int end;
 	enum token type;
-}
+	char* value;
+};
 
 int main(int argc, char *argv[]) {
 
 	return 0;
 }
 
-int* tokenize
+
+enum token getType(char* input, int index){//will return the token type from current index
+
+
+
+}
+void tokenize(char* input){
+	int length = 0;
+	length = strlen(input);
+	int start = 0; //first token must have index 0
+	int i;
+	for(i = 0; input[i]!='\0'; i++){
+		if()
+	}
+	return 0;
+}
