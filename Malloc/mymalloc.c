2788b1a40247627fb1cc24ea8fe92fb488f86394
Alborz Jelvani <aj654@kill.cs.rutgers.edu>
2020-10-21 17:27:34 -0400

    fixed bug in mymalloc

--- Malloc/mymalloc.c
+++ Malloc/mymalloc.c
@@ -5,11 +5,11 @@
 #include <stdio.h>
 #include "mymalloc.h"
 /*simulated memory, begins with first 2 bytes of a block as metadata in order:  size of block (int16_t) and the size is negative if block not used, positive if used*/
-static char vmem[4096] = {0};
+static char vmem[4096] = {0,0};
 
 
 void *mymalloc(size_t size, char* file, char* line){
-	if(vmem[0] == 0){//on first call, create initial metadata
+	if(vmem[0] == 0 && vmem[1] == 0){//on first call, create initial metadata
 		int16_t tmp = (int16_t) sizeof(vmem) -2;
 		tmp*=-1;
 		memcpy(&vmem[0],&tmp,2);
@@ -19,7 +19,7 @@ void *mymalloc(size_t size, char* file, char* line){
 	int16_t tmpsize = 0;
 	while(ptr < &vmem[4093]){//ptr here can only point to metadata bytes
 		if(*(int16_t*) ptr <0){//an unused prev allocated block
-			if(abs(*(int16_t*) ptr) > (size+2)){
+			if(abs(*(int16_t*) ptr) >= (size)){
 				tmpsize = (int16_t) size;
 				break;
 			}
@@ -35,7 +35,10 @@ void *mymalloc(size_t size, char* file, char* line){
 		char* tmp = ptr + tmpsize+2;
 		memcpy(ptr,&tmpsize,2);
 		ptr+=2;
-		memcpy(tmp,&newblock,2);
+		if((tmp+3) < &vmem[4095]){
+			memcpy(tmp,&newblock,2);
+		}	
+		
 	}
 	return ptr;
 }
@@ -49,7 +52,7 @@ void myfree(void* ptr,char* file, char* line){
 	while(current < &vmem[4093]){
 		if(current ==  ptr){
 			int16_t tmp = *(int16_t*) ptr;
-			if(tmp<0){
+			if(tmp<0){//address was not allocated by mymalloc
 				break;
 			}
 			tmp*=-1;
@@ -64,25 +67,13 @@ void myfree(void* ptr,char* file, char* line){
 /*
 void main(){
 
-	int* ptr = (int*) mymalloc(sizeof(int)*10,NULL,NULL);
-	ptr[0]= 5;
-	ptr[1] = 10;
-	int* ptr1 = (int*) mymalloc(sizeof(int)*100,NULL,NULL);
-	int* ptr2 = (int*) mymalloc(sizeof(int)*10,NULL,NULL);
-	int* ptr3 = (int*) mymalloc(sizeof(int)*100,NULL,NULL);
 
-	ptr1[0]= 443500;
-	ptr1[1] = 3453;
-	ptr2[0] = 1234;
-	ptr2[1] = 5678;
-	printf("%d %d\n",ptr[0],ptr[1]);
-	printf("%d %d\n",ptr1[0],ptr1[1]);
-	printf("%d %d\n",ptr2[0],ptr2[1]);
-	myfree(ptr2,"file","line");
-	ptr2 = (int*) mymalloc(sizeof(int)*100,NULL,NULL);
-	ptr2[0] = 1234;
-	ptr2[1] = 5678;
-	printf("%d %d\n",ptr2[0],ptr2[1]);
-	myfree(ptr2,"file","line");
+	for(int i=0;i<2000;i++){
+		
+		char* tmp = mymalloc(1,NULL,NULL);
+		myfree(tmp,NULL,NULL);
+	}
+
+
 }
 */
