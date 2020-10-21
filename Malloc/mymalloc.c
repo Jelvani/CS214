8ca3d39c5ac8869ec751b80285f6a3450070d918
Alborz Jelvani <aj654@kill.cs.rutgers.edu>
2020-10-21 01:58:18 -0400

    working on free, need to add metadat block regrouping

--- Malloc/mymalloc.c
+++ Malloc/mymalloc.c
@@ -3,11 +3,12 @@
 #include <stddef.h>
 #include <string.h>
 #include <stdlib.h>
+#include <stdio.h>
 /*simulated memory, begins with first 2 bytes of a block as metadata in order:  size of block (int16_t) and the size is negative if block not used, positive if used*/
 static char vmem[4096] = {0};
 
 
-void *mymalloc(size_t size){
+void *mymalloc(size_t size, char* file, char* line){
 	if(vmem[0] == 0){//on first call, create initial metadata
 		int16_t tmp = (int16_t) sizeof(vmem) -2;
 		tmp*=-1;
@@ -39,14 +40,36 @@ void *mymalloc(size_t size){
 	return ptr;
 }
 
+
+/*The free function will follow the metadata chain to see if given pointer matches any of the addresses*/
+void myfree(void* ptr,char* file, char* line){
+
+	char* current = vmem;
+	ptr-=2;
+	while(current < &vmem[4093]){
+		printf("current: %lp Pointer: %lp\n",current,ptr);
+		if(current ==  ptr){
+			int16_t tmp = *(int16_t*) ptr;
+			if(tmp<0){
+				break;
+			}
+			tmp*=-1;
+			memcpy(ptr,&tmp,2);
+			return;
+		}
+		current+= abs(*(int16_t*)current) +2;
+	}
+	printf("Error freeing pointer in %s at line %s\n",file,line);
+}
+
 void main(){
 
-	int* ptr = (int*) mymalloc(sizeof(int)*10);
+	int* ptr = (int*) mymalloc(sizeof(int)*10,NULL,NULL);
 	ptr[0]= 5;
 	ptr[1] = 10;
-	int* ptr1 = (int*) mymalloc(sizeof(int)*100);
-	int* ptr2 = (int*) mymalloc(sizeof(int)*10);
-	int* ptr3 = (int*) mymalloc(sizeof(int)*900);
+	int* ptr1 = (int*) mymalloc(sizeof(int)*100,NULL,NULL);
+	int* ptr2 = (int*) mymalloc(sizeof(int)*10,NULL,NULL);
+	int* ptr3 = (int*) mymalloc(sizeof(int)*100,NULL,NULL);
 
 	ptr1[0]= 443500;
 	ptr1[1] = 3453;
@@ -55,5 +78,11 @@ void main(){
 	printf("%d %d\n",ptr[0],ptr[1]);
 	printf("%d %d\n",ptr1[0],ptr1[1]);
 	printf("%d %d\n",ptr2[0],ptr2[1]);
-
+	myfree(ptr2,"file","line");
+	ptr2 = (int*) mymalloc(sizeof(int)*100,NULL,NULL);
+	ptr2[0] = 1234;
+	ptr2[1] = 5678;
+	printf("%d %d\n",ptr2[0],ptr2[1]);
+	myfree(ptr2,"file","line");
 }
+
