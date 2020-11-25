82f8ee5998301981c4c1f69375aba9bbf855ca16
Alborz Jelvani <aj654@kill.cs.rutgers.edu>
2020-11-25 15:54:05 -0500

    finally fixed some pointer stack bug, that modified that stack of another thread

--- FileAnalysis/fileanalyzer.c
+++ FileAnalysis/fileanalyzer.c
@@ -26,58 +26,80 @@ struct token{
 	struct token* next;
 };
 
-//according to menny, we cannot have our mutex as a global, so we pass this struct to each thread
-struct thread{
+//according to menny, we cannot have our mutex as a global, so we pass this struct to each thread argument
+struct threadArg{
 	char* dir;
 	pthread_mutex_t* lock;
+	struct file* head;
 };
 
+
+void* fileHandle(void* directory){
+	struct threadArg* args = (struct threadArg*) directory;
+	printf("FILE HANDLE: %s\n",args->dir);
+	return 0;
+}
 void* findDir(void* directory) {
-	struct thread* args = (struct thread*) directory;
+	struct threadArg* args = (struct threadArg*) directory;
     DIR *dir;
     struct dirent *dp;
-	pthread_t* thread_list = (pthread_t*) malloc(sizeof(pthread_t));
+	pthread_t* thread_list = (pthread_t*) malloc(sizeof(pthread_t));//array of thread id's
 	int trdCnt = 1;
-	printf("%s\n",args->dir);
     //checks to see if can open directory, if not fail it.
+	
     if((dir = opendir(args->dir)) == NULL) {
-		printf("ERR: cannot open current directory");
+		printf("ERR: cannot open %s\n",args->dir);
+		printf(RESET);
 		exit(1);
 	}
+
 	printf(RESET);
-    while((dp = readdir(dir)) != NULL) {
-        
+    while((dp = readdir(dir)) != NULL) { 
 
         if(dp->d_type == DT_DIR) {
-
 			//check for previous and current dir links
             if(strcmp(dp->d_name, ".") == 0 || strcmp(dp->d_name, "..") == 0) {
                 continue;
             }
-                printf(BLU "Found dir: %s\n", dp->d_name);
-                char* temp = (char*) malloc(strlen(args->dir) + strlen(dp->d_name) + 2);
-				strcpy(temp,args->dir);
-                strcat(temp, "/");
-                strcat(temp, dp->d_name);
-				struct thread new_args;
-				new_args.dir = temp;
-				
-				pthread_create(thread_list+trdCnt-1,NULL,findDir,(void*) &new_args);
-				trdCnt++;
-				thread_list = (pthread_t*) realloc(thread_list,sizeof(pthread_t)*trdCnt);
+			
+			struct threadArg* new_args = (struct threadArg*) malloc(sizeof(struct threadArg));
+            new_args->dir = (char*) malloc(strlen(args->dir) + strlen(dp->d_name) + 2);
+			strcpy(new_args->dir,args->dir);
+            strcat(new_args->dir, "/");
+            strcat(new_args->dir, dp->d_name);
+			new_args->lock = args->lock;
+			new_args->head = args->head;
+			printf(BLU "Found dir: %s\n", new_args->dir);
+			pthread_create(thread_list+trdCnt-1,NULL,findDir,(void*) new_args);
+			trdCnt++;
+			//dynamicly resize array
+			thread_list = (pthread_t*) realloc(thread_list,sizeof(pthread_t)*trdCnt);
 		
         } else if(dp->d_type == DT_REG) {//found a regular file
-			//check for exectuable later, not specified in instructions
+			//check for exectuable in file handler thread
+			struct threadArg* new_args = (struct threadArg*) malloc(sizeof(struct threadArg));
+            new_args->dir = (char*) malloc(strlen(args->dir) + strlen(dp->d_name) + 2);
+			strcpy(new_args->dir,args->dir);
+            strcat(new_args->dir, "/");
+            strcat(new_args->dir, dp->d_name);
+			new_args->lock = args->lock;
+			new_args->head = args->head;
 			printf(GRN "Found file: %s\n", dp->d_name);
-
+			pthread_create(thread_list+trdCnt-1,NULL,fileHandle,(void*) new_args);
+			trdCnt++;
+			//dynamicly resize array
+			thread_list = (pthread_t*) realloc(thread_list,sizeof(pthread_t)*trdCnt);
         }
         printf(RESET);
     }
     closedir(dir);
-
+	for(int i=0; i<trdCnt; i++){
+		pthread_join(thread_list[i],NULL);
+	}
     
 	free(args->dir);
 	free(thread_list);
+	return 0;
 }
 
 int main(int argc, char *argv[]) {
@@ -93,7 +115,7 @@ int main(int argc, char *argv[]) {
 	files.next=NULL;
 	files.token=NULL;
 	
-	struct thread args;
+	struct threadArg args;
 	args.lock = (pthread_mutex_t*) malloc(sizeof(pthread_mutex_t));
         //initializes mutex lock for synch
         if(pthread_mutex_init(args.lock, NULL) != 0) { 
@@ -108,7 +130,9 @@ int main(int argc, char *argv[]) {
 		printf(RESET);
 		return EXIT_FAILURE;
 	}
+	
 	args.dir = fullpath;
+	args.head = &files;
 		//does not need to be thread as per instruction (1.c)
 	
     findDir((void*) &args);
