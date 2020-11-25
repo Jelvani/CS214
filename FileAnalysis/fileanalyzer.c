be86e05dbb0383ac5a212723c399ce05d3b18a3a
Alborz Jelvani <aj654@kill.cs.rutgers.edu>
2020-11-25 00:48:24 -0500

    adde threading for directories

--- FileAnalysis/fileanalyzer.c
+++ FileAnalysis/fileanalyzer.c
@@ -30,56 +30,59 @@ struct token{
 struct thread{
 	char* dir;
 	pthread_mutex_t* lock;
-}
+};
 
-void* findDir(void* args) {
+void* findDir(void* directory) {
+	struct thread* args = (struct thread*) directory;
     DIR *dir;
     struct dirent *dp;
-    struct stat sb;
-    int vCounter = 0;
-    char* values[100];
-	printf("%s\n",directory);
+	pthread_t* thread_list = (pthread_t*) malloc(sizeof(pthread_t));
+	int trdCnt = 1;
+	printf("%s\n",args->dir);
     //checks to see if can open directory, if not fail it.
-    if((dir = opendir(directory)) == NULL) {
+    if((dir = opendir(args->dir)) == NULL) {
 		printf("ERR: cannot open current directory");
 		exit(1);
 	}
-
+	printf(RESET);
     while((dp = readdir(dir)) != NULL) {
-        printf(RESET);
+        
 
         if(dp->d_type == DT_DIR) {
+
 			//check for previous and current dir links
             if(strcmp(dp->d_name, ".") == 0 || strcmp(dp->d_name, "..") == 0) {
                 continue;
-            } else {
+            }
                 printf(BLU "Found dir: %s\n", dp->d_name);
-                char* temp = directory;
+                char* temp = (char*) malloc(strlen(args->dir) + strlen(dp->d_name) + 2);
+				strcpy(temp,args->dir);
                 strcat(temp, "/");
                 strcat(temp, dp->d_name);
-                values[vCounter++] = temp;
-            }
+				struct thread new_args;
+				new_args.dir = temp;
+				
+				pthread_create(thread_list+trdCnt-1,NULL,findDir,(void*) &new_args);
+				trdCnt++;
+				thread_list = (pthread_t*) realloc(thread_list,sizeof(pthread_t)*trdCnt);
 		
-        } else {
-            //skip executables since we cannot read them
-            if(stat(dp->d_name, &sb) == 0 && sb.st_mode & S_IXUSR) {
-				continue;
-			} else { 
-				printf(GRN "Found file: %s\n", dp->d_name);
-			}
+        } else if(dp->d_type == DT_REG) {//found a regular file
+			//check for exectuable later, not specified in instructions
+			printf(GRN "Found file: %s\n", dp->d_name);
+
         }
         printf(RESET);
     }
     closedir(dir);
 
-    for(int i = 0; i < vCounter; i++) {
-        findDir(values[i]);
-    }
+    
+	free(args->dir);
+	free(thread_list);
 }
 
 int main(int argc, char *argv[]) {
     //Checks for args count
-    if(argc != 2) {
+    if(argc != 2){
         printf(RESET);
         printf("ERR: INVALID ARG COUNT\nTo properly run the program, enter a directory to start scanning.\n");
         return EXIT_FAILURE;
@@ -89,7 +92,9 @@ int main(int argc, char *argv[]) {
 	struct file files;
 	files.next=NULL;
 	files.token=NULL;
-	struct threads args;
+	
+	struct thread args;
+	args.lock = (pthread_mutex_t*) malloc(sizeof(pthread_mutex_t));
         //initializes mutex lock for synch
         if(pthread_mutex_init(args.lock, NULL) != 0) { 
             printf("mutex init has failed\n"); 
@@ -97,16 +102,16 @@ int main(int argc, char *argv[]) {
         } 
 
     char* fullpath;
+	
 	if((fullpath=realpath(argv[1], NULL))==NULL){
 		printf(RED "The path: '%s' is invalid!\n",argv[1]);
 		printf(RESET);
 		return EXIT_FAILURE;
 	}
-	
 	args.dir = fullpath;
 		//does not need to be thread as per instruction (1.c)
-    findDir(&args);
-    free(fullpath);//realpath uses malloc, so we must deallocate used memory
+	
+    findDir((void*) &args);
     printf(RESET);
     return EXIT_SUCCESS;
 }
