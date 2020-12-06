c92ee4ad7d50805fa73fa6876df0bfaf5f09befe
Alborz Jelvani <aj654@kill.cs.rutgers.edu>
2020-12-05 20:48:06 -0500

    simple working REG message checker

--- WhosThere/Asst3.c
+++ WhosThere/Asst3.c
@@ -5,7 +5,7 @@
 #include <sys/types.h>
 #include <sys/socket.h>
 #include <netdb.h>
-
+#define KKJ_L 8
 
 
 
@@ -13,11 +13,56 @@
 
 
 char* getKKJ(char* payload, int* length){//returns string with implemented KKJ protocl
-	*length = strlen(payload) +9;
-	char* message = (char*) malloc(*length);
-	sprintf(message,"REG|%d|%s|\n",*length-9,payload);
-	//8 here signifies our required format byte length without payload: REF|##|blahblah|
+	*length = strlen(payload) +KKJ_L+2;
+	char* message = (char*) malloc(*length)+2;
+	sprintf(message,"REG|%d|%s|\n",*length-KKJ_L,payload);
+	//8 here signifies our required format byte length without payload: REG|##|blahblah|
 	
+	return message;
+}
+char* readMessage(int fd){//reads given size bytes from buffer
+	int seen = 0;
+	char header[4];
+	char charLen[3];
+	char byte = 0;
+	char pipe = '0';
+	for(int i =0;i<4;i+=read(fd,&header[i],1)){
+
+	}
+	if(strncmp(header,"REG|",4)!=0){//message already doesnt conform to REG|
+		return NULL;
+	}
+
+	for(int i =0;i<2;i+=read(fd,&charLen[i],1)){
+
+	}
+	charLen[2] = '\0';
+	int len = atoi(charLen);
+	if(len==0){//not 2 digits
+		return NULL;
+	}
+	for(int i =0;i<1;i+=read(fd,&pipe,1)){
+
+	}
+	if(pipe!='|'){
+		return NULL;
+	}
+	pipe = '0';
+	char* message = (char*) malloc(len+1);
+	for(int i =0;i<len;i+=read(fd,&message[i],1)){
+		if(message[i]=='|'){
+			printf("INVALID MESSAGE LENGTH!\n");
+			return NULL;
+		}
+	}
+	message[len] = '\0';
+	for(int i =0;i<1;i+=read(fd,&pipe,1)){
+
+	}
+	if(pipe!='|'){
+		return NULL;
+	}
+	//if we get here, we have received a valid message, so we return the message
 	return message;
 }
 
@@ -82,6 +127,12 @@ int main(int argc, char *argv[]) {
 		int len = 0;
 		char* string = getKKJ("Knock, knock.",&len);
 		write(fd_client,string,len);
+		char* m1 = readMessage(fd_client);
+		if(m1!=NULL){
+			printf("%s\n",m1);
+		}
+	
+
 		close(fd_client);
 		break;
 
